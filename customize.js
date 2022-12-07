ba6bbb4f5f90c064b181f99aa24b5d72b5d0658d
sy_hammer <109604471+syhammer@users.noreply.github.com>
2022-12-07 09:24:48 -0500

    Create customize.js
    Allow for customization of the thermometer instance.

---
+++ customize.js
@@ -0,0 +1,8 @@
+function CustomizeObject(thermometer)
+{
+  thermometer.bar.size.inner.x = 300;
+  thermometer.bar.size.inner.y = 6;
+  thermometer.bar.size.outer.x = width;
+  thermometer.bar.size.outer.y = height;
+  thermometer.bar.borderRadius = 3;
+}
